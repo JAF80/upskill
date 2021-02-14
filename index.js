67d0bdffa643d0c58b32a7af83db9513ba45618a
Jose Ferreira <josealexan@gmail.com>
2021-02-14 17:23:34 +0000

    Update index.js
    Erro na query resolvido

--- index.js
+++ index.js
@@ -31,15 +31,15 @@ function postRoot(req,res){
 
 function getUsers(req,res){
     console.log("entrei na rota /users com o verbo http get");
-    connection.query(`select name,age from utilizadores`)
+    connection.query("select name,age from utilizadores");
     res.send("{ list of users }");   
 }
 
 function postUser(req,res){
     console.log("entrei na rota /users com o verbo http post");
     res.send("POST request to users");   
-    console.log (`insert into utilizadores (name, age) values ("${req.body.username}",${req.body.age})`);
-    connection.query(`insert into utilizadores (name, age) values ("${req.body.username}",${req.body.age})`)
+    console.log ("insert into students (name, age) values ('"+req.body.username+"',"+req.body.age+")");
+    connection.query("insert into students (name, age) values ('"+req.body.username+"',"+req.body.age+")");
 }
 
 app.route("/users")
@@ -50,24 +50,24 @@ app.route("/")
     .get(getRoot)
     .post(postRoot);
 
-app.get("/topic", (req, res) => {
+app.get("/topic", function myFC1(req, res) {
     console.log("entrei na rota /topic com o verbo http get");
     res.send([1,2,3])
 });
 
-app.get("/topic/:id", (req, res) => {
+app.get("/topic/:id", myFC2(req, res) {
     console.log("entrei na rota /topic/:id com o verbo http get");
     res.send([1 ])
 });
 
-app.listen (8080, () => {
+app.listen (8080, myFC3() {
         console.log("listen on port 8080");
         connection = mysql.createConnection({
          host: 'localhost',
          user: 'myappuser',
          password: 'mypass',
          database: 'test',
-            port: 3306
+         port: 3306
        });
         connection.connect(function(err){
         if (err){
